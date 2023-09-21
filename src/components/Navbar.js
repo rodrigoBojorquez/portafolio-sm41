203431b5a764a7e909ec83376aebcda6f5600137
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2023-09-21 16:09:17 -0500

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,12 @@ function Navbar() {
         <nav className='fixed w-24 h-full top-0 left-0 bg-[#3F72AF] py-5'>
 
             {/* home  */}
-            <a href='#'><img src={homeIcon} className='m-auto'/></a>
+            <a href='#'><img src={homeIcon} className='m-auto transform duration-500 hover:scale-105'/></a>
             <hr className='border-2 border-[#DBE2EF] m-auto w-10/12 my-5 '/>
 
             <ul className='flex flex-col items-center gap-3'>
                 {/* ir añadiendo los links de otras rutas aqui  */}
-                <li><a href='#'><img src={weatherIcon}/></a></li>
+                <li><a href='#'><img src={weatherIcon} className='transform duration-500 hover:scale-105'/></a></li>
             </ul>
         </nav>
     )
