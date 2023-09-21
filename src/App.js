fd47c77d21195a13b0edeff525e132cdbed8a51e
rodrigo_bojorquez <rbojorquez1620@gmail.com>
2023-09-21 15:14:44 -0500

    se agrego tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
     <>
-      Proyecto
+      <Navbar/>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a>Example</a></li>
+                <li><a>Example</a></li>
+                <li><a>Example</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
